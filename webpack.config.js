d0517a72f3f8d8b8171a628a54e6e2e0e74299b1
Rolaxx <gaelelpapi@gmail.com>
2019-05-22 11:55:28 -0400

    configuracion solo webpack

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        main: path.resolve(__dirname,"public/js/main.js")
+    },
+    output: {
+      path: path.resolve(__dirname,'dist'),
+      filename: 'src/js/[name][hash].js'
+    },module:{       
+        rules:[{
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {              
+                  publicPath: '../'
+                }
+              },
+              "css-loader"]
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {                  
+                  publicPath: '../'
+                }
+              },
+              "css-loader","sass-loader"
+            ]
+          },         
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+         {
+          test: /\.(png|jpg|gif|woff|eot|ttf|svg)$/i,
+          use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 5000000,
+                  name : '[name].[hash].[ext]'
+                }
+              }
+            ]
+
+      },
+      {
+        test: /\.(mp4|webm)$/i,
+        use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 5000000,
+                name : '[name].[ext]'
+              }
+            }
+          ]
+
+    }
+
+        ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template:'./index.html',
+          filename: './index.html'
+      }),
+      //new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "./src/css/[name].[hash].css",
+        chunkFilename: "css/[id].[hash].css"
+      })
+    ],
+    devServer: {},
+  }
